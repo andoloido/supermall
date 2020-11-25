7fb7aec24ecc5c00ec699c1c749284afcd376485
sunzhiyuan <sunzhiyuan1997@gmail.com>
2020-11-25 09:31:35 +0800

    home head completed

---
+++ src/network/home.js
@@ -0,0 +1,28 @@
+import {request} from "./request";
+
+export function getHomeMultidata() {
+  return request({
+    url: '/home/multidata'
+  })
+}
+
+export function getHomeGoods(type, page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
+  })
+}
+
+let totalNums = []
+
+const nums1 = [20, 11, 222]
+const nums2 = [111, 22, 333]
+
+// for (let n of nums1) {
+//   totalNums.push(n)
+// }
+
+totalNums.push(...nums1)
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"> <div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <home-recommend-view :recommendList="recommendList" />
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+import { getHomeMultidata } from "network/home";
+
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "../home/childComps/HomeSwiper";
+import HomeRecommendView from "./childComps/HomeRecommendView.vue";
+
+export default {
+  components: { NavBar, HomeSwiper, HomeRecommendView },
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommendList: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommendList = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
